8150b2f2f47a5569ba4ed2bd48b200472c449de3
David Tauraso <david318@me.com>
2026-05-22 18:02:56 -0700

    refactor(test): remove unused instantStub function

--- nodes/Wiring/paced_wire_test.go
+++ nodes/Wiring/paced_wire_test.go
@@ -7,24 +7,6 @@ import (
 	"time"
 )
 
-// instantStub auto-signals delivery and done whenever a value is placed,
-// simulating a buffered-1 chan without a visual layer.
-func instantStub(pw *PacedWire) {
-	go func() {
-		for {
-			time.Sleep(time.Millisecond)
-			pw.mu.Lock()
-			hasSend := pw.hasSend
-			pw.mu.Unlock()
-			if hasSend {
-				pw.NotifyDelivered()
-				time.Sleep(time.Millisecond)
-				pw.Done()
-			}
-		}
-	}()
-}
-
 func TestSendRecvInstantDelivery(t *testing.T) {
 	pw := NewPacedWire()
 	ctx := context.Background()
